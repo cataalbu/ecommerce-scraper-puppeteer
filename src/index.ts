404daa3fe7546e7addd6d56caa1910bd52677ed0
Catalin Albu <cataalbu1919@gmail.com>
2024-02-24 18:10:17 +0200

    fix: Build commnad was not working properly

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
 import scrapeSSREcommerceWebsite from './scrapers/SSRScraper/index.js';
 import scrapeCSREcommerceWebsite from './scrapers/CSRScraper/index.js';
-
-// (async () => {
-//   const { products, ...result } = await scrapeSSREcommerceWebsite();
-//   console.log(result);
-// })();
-
-// (async () => {
-//   const { products, ...result } = await scrapeCSREcommerceWebsite();
-//   console.log(result);
-// })();
